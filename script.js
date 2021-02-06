ef1bb433ea1041b14546cf68e71fcdc50d6ea334
smoloff <smoloff@gmail.com>
2021-02-06 12:45:50 +0200

    class work

---
+++ script.js
@@ -0,0 +1,18 @@
+let password='';
+let username='';
+
+const f = (x) => {
+    return x * x;
+}
+
+// let x = prompt('Ente x', '');
+// alert(x);
+// let result = f(x);
+// console.log(result);
+
+// $(document).ready(function() {
+//     console.log('DOcument is ready')
+// })
+const logIn = () => {
+    console.log('on press')
+}
